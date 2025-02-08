38cc8539a2ef211c67dd95e4c26b052363e09117
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2025-02-08 18:31:42 +0000

    Added datatype file

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" //treat all the JS code as newer version.
+
+// alert("Hello"); //we are using nodejs, not browser.
+
+console.log(3 
+    + 3);  //Code readability should be high
+console.log("Nagen");
+
+let name = "NAGEN"
+let age = 31
+let isLoggedIn = false 
+
+//Primitive datatypes
+
+// number => 2 to the power of 53, 
+// bigInt
+// string => " "
+// bopolean => true / false 
+// null => standalone value, it's empty value 
+// undefined => value is not defined.
+// symbol => Unique 
+
+
+// Objects 
+
+console.log(typeof "nagen");
+console.log(typeof 23);
+console.log(typeof name);
+
+console.log(typeof null); //objects
+
+console.log(typeof undefined); //undefined 
