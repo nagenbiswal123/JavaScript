c33a42a24517516ffac76d9f4451d979242ba792
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2025-02-09 03:03:55 +0000

    added conversion file

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "23abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber) //
+
+console.log(typeof valueInNumber) //
+
+// let score2 = null
+// let score2 = undefined 
+let score2 = "Nagen";
+
+
+let valueInNumber2 = Number(score2)
+
+console.log(typeof score2)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false =>0
+// " " =>false
+
+// "Nagen" => false
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = " Nagen"
+
+let booleanisLoggedIn  = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn)
+
+
+// 1 => true; 0 => false
+// " " => false
+// " Nagen" =>true c 
+
+
+let someNumber  = 23 //
+let stringNumber = String(someNumber); //it's converted to string.
+console.log(stringNumber); //23
+
+console.log(typeof stringNumber) //string
+
+
