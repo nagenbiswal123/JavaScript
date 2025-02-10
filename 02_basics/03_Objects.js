29c7a515bfa6e1f361849247564d864eca70e541
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2025-02-10 12:14:13 +0000

    content added

--- 02_basics/03_Objects.js
+++ 02_basics/03_Objects.js
@@ -7,7 +7,7 @@ const mySym = Symbol("key1");
 //Object literals 
 const JsUser = {
     name : "Nagen",
-    mySym : "myKey1",
+    [mySym] : "myKey1",
     "fullname" : " NAGEN BISWAL",
     age : 29,
     email : "nagen.google.com",
@@ -23,3 +23,37 @@ console.log(JsUser.fullname);
 console.log(JsUser["fullname"]);
 console.log(JsUser.mySym); //myKey1
 
+
+JsUser.email = "biswalnagen02@gmail.com";
+
+// Object.freeze(JsUser); //It will freeze the value.
+JsUser.email = " google@gmail.com"
+console.log(JsUser);
+
+
+
+// Functions 
+// Getting add
+
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+
+
+console.log(JsUser.greeting)  //[Function (anonymous)]
+console.log(JsUser.greeting()) //Hello JS user
+
+console.log("++++++++++++******************++++++++++++");
+
+
+JsUser.greetingTwo = function() {
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greetingTwo());
+console.log(JsUser.greetingTwo());
+
+
+
+// JsUser.name
+// JsUser[username]
