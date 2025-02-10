3a69a32679ec9b099bb4520d3372ca524df6b5f7
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2025-02-10 16:00:34 +0000

    added contnt

--- 03_Functions/02_scopes.js
+++ 03_Functions/02_scopes.js
@@ -11,16 +11,13 @@ if (true) {
     //Block scopes 
     let a = 23
     const b = 34
-    console.log("Inner: ", a);
-    
-
+    console.log("Inner: ", a); 
 }
+
 
 console.log(a);
 // console.log(b);
 // console.log(c);    // 87
-
-
 
 
 for(let i = 0; i < 10; i++){
@@ -31,10 +28,43 @@ for(let i = 0; i < 10; i++){
 }
 
 
-
 let arr = [1, 2, 3, 5, 6];
 
 for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
     console.log(element); // This will print each element in the array
 }
+
+
+//Nested Scope... //
+
+
+function one() {
+    const username = "nagen"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "nagen"
+    if (username === "nagen") {
+        const website = "youtube"
+        console.log(username + website);   // nagenyoutube
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+console.log("++++++++++++++++********************************++++++++++++++++");
+
+
