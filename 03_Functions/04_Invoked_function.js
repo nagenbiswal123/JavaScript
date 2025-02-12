a68892fcc142942320998cd9670b1ca5aaba6011
Nagen Biswal <104751344+nagenbiswal123@users.noreply.github.com>
2025-02-12 13:08:52 +0000

    file added

--- 03_Functions/04_Invoked_function.js
+++ 03_Functions/04_Invoked_function.js
@@ -7,7 +7,6 @@ function chai() {
 chai();
 
 
-
 (function job(){
     console.log(`Thanks Bhai Log !!`);
 })();    //This is called IIFE
@@ -20,3 +19,27 @@ chai();
 })("nagen");
 
 //
+
+let val1 = 123;
+let val2 = 376;
+
+
+function addTwo(num1, nmum2) {
+    let sum = num1 + nmum2;
+    return sum;
+}
+
+let result = addTwo(val1, val2);
+
+let resul23 = addTwo(23,12);
+
+console.log(result);
+
+console.log((resul23));
+
+console.log(typeof val1);
+
+
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+
